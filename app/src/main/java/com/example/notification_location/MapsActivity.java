e7703c7b0503e57869886e50434020a29597110d
LocationNotifier <anjus9808@gmail.com>
2023-03-11 16:45:36 +0530

    Location notification icon changed

--- app/src/main/java/com/example/notification_location/MapsActivity.java
+++ app/src/main/java/com/example/notification_location/MapsActivity.java
@@ -69,21 +69,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 10, (LocationListener) this);
-        } else {
-            Toast.makeText(this, "Please enable GPS", Toast.LENGTH_SHORT).show();
-        }
+
         locationListener = new LocationListener() {
 
             @Override
